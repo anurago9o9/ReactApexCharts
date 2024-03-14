e854b56d599f4306c36424cdf48fde3b3563456c
anurago9o9 <58781750+anurago9o9@users.noreply.github.com>
2024-03-14 22:55:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Chart from 'react-apexcharts';
+import axios from 'axios'; 
 
 function App() {
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    axios.get("https://checkinn.co/api/v1/int/requests")
+      .then(response => {
+        const { requests } = response.data;
+        const requestsPerHotel = {};
+
+        requests.forEach(request => {
+          const hotelName = request.hotel.name;
+          if (requestsPerHotel.hasOwnProperty(hotelName)) {
+            requestsPerHotel[hotelName]++;
+          } else {
+            requestsPerHotel[hotelName] = 1;
+          }
+        });
+
+        // Prepare data for chart
+        const chartOptions = {
+          chart: {
+            id: "requests-line"
+          },
+          xaxis: {
+            categories: Object.keys(requestsPerHotel)
+          },
+          yaxis: {
+            min: 0,
+            tickAmount: Object.keys(requestsPerHotel).length 
+          }
+        };
+
+        const chartSeries = [
+          {
+            name: "Requests",
+            data: Object.values(requestsPerHotel)
+          }
+        ];
+
+        setChartData({ options: chartOptions, series: chartSeries });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {chartData && chartData.series && (
+          <Chart
+            options={chartData.options}
+            series={chartData.series}
+            type="line"
+            width="800"
+          />
+        )}
       </header>
     </div>
   );
